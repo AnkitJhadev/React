d9b9c324e62a11fcffbe5a2e507933d926eb1167
AnkitJhadev <jha80250@gmail.com>
2024-01-02 16:05:59 +0530

    a

---
+++ App.js
@@ -0,0 +1,11 @@
+import ExpenseItem from "./Components/Expenseitm";
+function App() {
+  return (
+    <div >
+   <h2>LETS GET STARTED</h2>
+   <ExpenseItem/>
+    </div>
+  );
+}
+
+export default App;
